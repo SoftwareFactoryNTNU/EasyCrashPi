ccfb86efd4cb40bec17140d13fa4c438465b2ea3
Mathias Aarseth Pedersen <mathiaap@stud.ntnu.no>
2016-02-10 16:07:48 +0100

    Created maven project

---
+++ BlackboxPi/src/main/java/DigitalUnit/App.java
@@ -0,0 +1,13 @@
+package DigitalUnit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
