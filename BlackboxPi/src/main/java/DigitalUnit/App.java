6dcdcb9e8cd3a9a98693f873e0f750e501ca1446
Mathias Aarseth Pedersen <mped@mimer.no>
2016-02-18 10:59:11 +0100

    Commiting changes for merge

--- BlackboxPi/src/main/java/DigitalUnit/App.java
+++ BlackboxPi/src/main/java/DigitalUnit/App.java
@@ -1,5 +1,9 @@
 package DigitalUnit;
 
+import java.io.IOException;
+
+import server.HttpServer;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        HttpServer serv = new HttpServer();
+        try {
+			serv.getPage("http://www.google.no");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        serv.printTest();
     }
 }
