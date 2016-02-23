3a59290d9b74830b5bdfc0cb40b39cae27808556
karsy <vegard.karstang@gmail.com>
2016-02-23 10:50:32 +0100

    Added table creation.

--- BlackboxPi/src/main/java/DigitalUnit/Database/DBClient.java
+++ BlackboxPi/src/main/java/DigitalUnit/Database/DBClient.java
@@ -1,29 +1,51 @@
 package DigitalUnit.Database;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
- * Created by vegard on 18.02.2016.
+ * Database client class for the derby database
  */
+
 public class DBClient {
 
     private Connection conn;
 
+    /**
+     * Establises a connection to the database
+     */
+
     public void connect() {
         try {
-            File dbfolder = new File(System.getProperty("user.home") + ".blackbox");
+            File dbfolder = new File(System.getProperty("user.home") + "/.blackbox");
             if (!dbfolder.exists()) {
                 conn = DriverManager.getConnection("jdbc:derby:" + System.getProperty("user.home") + "/.blackbox;create = true");
             } else {
                 conn = DriverManager.getConnection("jdbc:derby:" + System.getProperty("user.home") + "/.blackbox");
             }
-            System.out.println(conn.isClosed());
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    public void createTable() {
+        String tableString = "CREATE TABLE MEASUREMENTS (" +
+                "ID INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY" +
+                "(START WITH 1, INCREMENT BY 1)," +
+                "LATITUDE DOUBLE," +
+                "LONGITUDE DOUBLE," +
+                "VEHICLE_SPEED DOUBLE," +
+                "ENGINE_SPEED INTEGER," +
+                "ACCELERATOR_PEDAL DOUBLE," +
+                "BREAKING_PEDAL BOOLEAN," +
+                "TIMESTMP TIMESTAMP)";
+
+        try {
+            Statement statement = conn.createStatement();
+            statement.execute(tableString);
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
