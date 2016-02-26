cd0e047560e050889679e58261194bbbd9a42731
karsy <vegard.karstang@gmail.com>
2016-02-26 10:25:19 +0100

    Added json deserialization.

--- BlackboxPi/src/main/java/DigitalUnit/Car/CarSimulator.java
+++ BlackboxPi/src/main/java/DigitalUnit/Car/CarSimulator.java
@@ -1,25 +1,51 @@
 package DigitalUnit.Car;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import DigitalUnit.Car.CarListener;
+import com.google.gson.Gson;
 
 /**
- * Created by vegard on 16.02.16.
+ *
  */
-public class CarSimulator {
+
+public class CarSimulator implements Runnable {
 
     private CarListener carListener;
     private File jsonFile;
+    private List<CarData> carDataList = new ArrayList<>();
 
     public CarSimulator(CarListener carListener, String datasetLocation) {
         this.carListener = carListener;
         this.jsonFile = new File(datasetLocation);
-        System.out.println(jsonFile.exists());
-        System.out.println(jsonFile.getAbsolutePath());
+
+        if (jsonFile.exists()) {
+            deserializeJSON();
+        }
+    }
+
+    private void deserializeJSON() {
+        Gson gson = new Gson();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(jsonFile));
+            String in;
+            while ((in = bufferedReader.readLine()) != null) {
+                CarData carData = gson.fromJson(in, CarData.class);
+                carDataList.add(carData);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         CarSimulator sim = new CarSimulator(null, "res/JSON/downtown-west.json");
     }
 
+    @Override
+    public void run() {
+
+    }
 }
