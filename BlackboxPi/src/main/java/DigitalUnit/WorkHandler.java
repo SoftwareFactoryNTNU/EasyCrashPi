def82bfa9b3d9b07d4c2b3e121e6518a623d46c8
Stian Hanssen <Hanssen.Stian@gmail.com>
2016-02-28 22:13:34 +0100

    Changed some method names and added JavaDoc

--- BlackboxPi/src/main/java/DigitalUnit/WorkHandler.java
+++ BlackboxPi/src/main/java/DigitalUnit/WorkHandler.java
@@ -1,8 +1,19 @@
 package DigitalUnit;
 
+import java.util.ArrayList;
+
+import DigitalUnit.Data.CarData;
+import DigitalUnit.Database.DBClient;
+
 public class WorkHandler {
-	boolean regularState = false;
+	boolean regularState = true;
 	
+	/**Called upon when a complete carData object has been created. 
+	 * Evaluates data to set system in correct state and sends data to database.
+	 * If system recognizes a crash situation it will send data to server as well
+	 * 
+	 * @param data		CarData object representing a complete line of data from the car
+	 */
 	public void processData(CarData data) {
 		if (regularState) {
 			normalState(data);
@@ -16,7 +27,7 @@ public class WorkHandler {
 		DBClient.insert(data);
 		if (DataAnalyser.evalateData()) {
 			ArrayList<CarData> dataSet = DBClient.getAll();
-			SClient.sendSet(dataSet);
+			SClient.insertAll(dataSet);
 			regularState = false;
 		}
 	}
